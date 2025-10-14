663aaf59136c5c2aa45df98956e4584d3dccd176
w enxig <wenxinguo12@yeah.net>
2025-10-14 22:29:25 +0800

    chore: toRef change to official

--- lib/utils/layout.ts
+++ lib/utils/layout.ts
@@ -1,6 +1,5 @@
-import { toRef } from "@vueuse/core"
 import { last, remove } from "lodash-es"
-import { computed, onUnmounted, shallowReactive, watch, type MaybeRefOrGetter, type ComputedRef } from "vue"
+import { computed, onUnmounted, shallowReactive, watch, type MaybeRefOrGetter, type ComputedRef, toRef } from "vue"
 import { useGlobalVar } from "./plugin"
 
 const allLayers = useGlobalVar(shallowReactive<symbol[]>([]), 'utils/layers')
