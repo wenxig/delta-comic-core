462a3a567f175c65c75ac3f3f7c3c9e85b931a98
wenxig <wenxinguo12@gmail.com>
2025-12-14 00:14:29 +0800

    chore: share function multiple define

--- lib/utils/eventBus.ts
+++ lib/utils/eventBus.ts
@@ -1,6 +1,6 @@
 import type { PluginConfig } from "@/plugin/define"
 import type { uni } from "@/struct"
-import { random, uniqBy } from "es-toolkit/compat"
+import { random } from "es-toolkit/compat"
 import mitt from "mitt"
 import { useGlobalVar } from "./plugin"
 
@@ -13,7 +13,7 @@ export type EventBus = {
 export const eventBus = useGlobalVar(mitt<EventBus>(), 'utils/eventBus')
 
 export type SharedFunctions = {
-  getRandomProvide(signal: AbortSignal): PromiseLike<uni.item.Item[]>
+  getRandomProvide(signal?: AbortSignal): PromiseLike<uni.item.Item[]>
 
   addPlugin(ins: PluginConfig): void
 
@@ -37,20 +37,25 @@ export class SharedFunction {
   }[]>, 'utils/SharedFunction/sharedFunctions')
   public static define<TKey extends keyof SharedFunctions>(fn: SharedFunctions[TKey], plugin: string, name: TKey) {
     console.log('[SharedFunction.define] defined new function', plugin, ":", name, '->', fn)
-    this.sharedFunctions.set(name, uniqBy([...(this.sharedFunctions.get(name) ?? []), {
+    this.sharedFunctions.set(name, [...(this.sharedFunctions.get(name) ?? []), {
       fn,
       plugin
-    }], v => v.plugin))
+    }])
     return fn
   }
   public static call<TKey extends keyof SharedFunctions>(name: TKey, ...args: Parameters<SharedFunctions[TKey]>) {
-    return this.sharedFunctions.get(name)?.map(v => {
+    const ins = this.sharedFunctions.get(name)?.map(v => {
       const result: ReturnType<SharedFunctions[TKey]> = (<any>v.fn)(...args)
       return {
         result,
         ...v
       }
-    })
+    }) ?? []
+    const results = Promise.all(ins.map(async v => ({
+      ...v,
+      result: await v.result
+    })))
+    return Object.assign(ins, results)
   }
   public static callRandom<TKey extends keyof SharedFunctions>(name: TKey, ...args: Parameters<SharedFunctions[TKey]>) {
     const all = this.sharedFunctions.get(name) ?? []
@@ -59,19 +64,32 @@ export class SharedFunction {
     if (!it) throw new Error(`[SharedFunction.callRandom] call ${name}, but not resigner any function.`)
     console.log(`[SharedFunction.callRandom] call index: ${index} in ${all.length}`, it)
     const result: ReturnType<SharedFunctions[TKey]> = (<any>it.fn)(...args)
-    return {
+    const ins = {
       result,
       ...it
     }
+    const results = (async () => ({
+      ...it,
+      result: await result
+    }))()
+    return Object.assign(ins, results)
   }
   public static callWitch<TKey extends keyof SharedFunctions>(name: TKey, plugin: string, ...args: Parameters<SharedFunctions[TKey]>) {
     const all = this.sharedFunctions.get(name) ?? []
-    const it = all.find(c => c.plugin === plugin)
-    if (!it) throw new Error(`[SharedFunction.callWitch] not found plugin function (plugin: ${plugin}, name: ${name})`)
-    const result: ReturnType<SharedFunctions[TKey]> = (<any>it.fn)(...args)
-    return {
-      result,
-      ...it
-    }
+    const them = all.filter(c => c.plugin === plugin)
+    if (!them.length) throw new Error(`[SharedFunction.callWitch] not found plugin function (plugin: ${plugin}, name: ${name})`)
+
+    const ins = them.map(v => {
+      const result: ReturnType<SharedFunctions[TKey]> = (<any>v.fn)(...args)
+      return {
+        result,
+        ...v
+      }
+    })
+    const results = Promise.all(ins.map(async v => ({
+      ...v,
+      result: await v.result
+    })))
+    return Object.assign(ins, results)
   }
 }
