246609152c91c2801d0b65ba5b968f2d8610f3cf
w enxig <wenxinguo12@yeah.net>
2025-12-24 20:07:45 +0800

    fix: image super

--- lib/struct/image.ts
+++ lib/struct/image.ts
@@ -17,7 +17,7 @@ export class Image extends Resource {
   public static override create(v: RawResource | RawImage, aspect?: { width: number, height: number }): Image {
     return new this(v, aspect)
   }
-  protected constructor(v: RawResource | RawImage, public aspect?: { width: number, height: number }) {
+  protected constructor(v: RawResource | RawImage, aspect?: { width: number, height: number }) {
     if ('forkNamespace' in v) super({
       $$plugin: v.$$plugin,
       $$meta: v.$$meta,
@@ -26,6 +26,8 @@ export class Image extends Resource {
       processSteps: v.processSteps
     })
     else super(v)
+    this.aspect = aspect
   }
+  public aspect?: { width: number, height: number }
 }
 export type Image_ = string | Image
