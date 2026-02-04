ea846422b6f07de046f1da61f75cb873cda70317
w enxig <wenxinguo12@yeah.net>
2026-02-04 20:16:20 +0800

    fix(pub): ci config

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -1,7 +1,5 @@
-import { GlobalConfig } from 'semantic-release'
-
 import pkg from './package.json' with { type: 'json' }
-
+/** @type {import("semantic-release").GlobalConfig} */
 export default {
   branches: ['main'],
   repositoryUrl: pkg.repository.url,
@@ -9,14 +7,7 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        pkgRoot: '.', // 若你的 package.json 在项目根以外（例如构建产物在 dist），可改为 "dist"
-        tarballDir: 'dist' // 可选：把生成的 tgz 写到某目录用于其他用途
-      }
-    ],
+    ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.', tarballDir: 'dist' }],
     '@semantic-release/github',
     [
       '@semantic-release/git',
@@ -27,4 +18,4 @@ export default {
     ]
   ],
   tagFormat: '${nextRelease.version}'
-} as GlobalConfig
+}
