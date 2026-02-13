aac41994c2ef79e9afc0f32695628647c8252e49
w enxig <wenxinguo12@yeah.net>
2026-02-13 13:54:57 +0800

    fix(ci): [f] publish

--- release.config.mjs
+++ release.config.mjs
@@ -1,21 +1,29 @@
 import pkg from './package.json' with { type: 'json' }
-/** @type {import("semantic-release").GlobalConfig} */
+
+const includeChangelog = process.env.SEM_CHANGES === '1'
+
 export default {
   branches: ['main'],
   repositoryUrl: pkg.repository.url,
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.', tarballDir: 'dist' }],
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
+    // 仅在最终 changelog job 中加入下面两个插件
+    ...(includeChangelog
+      ? [
+          ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+          [
+            '@semantic-release/git',
+            {
+              assets: ['package.json', 'CHANGELOG.md', '**/package.json'],
+              message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+          ]
+        ]
+      : [
+          ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.', tarballDir: 'dist' }],
+          '@semantic-release/github'
+        ])
   ],
   tagFormat: '${version}'
 }
